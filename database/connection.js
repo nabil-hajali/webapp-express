c229c3e8fa7c88d39b8f4d2927a9017257a4913e
nabil-hajali <nabil2491@gmail.com>
2026-04-27 20:17:39 +0200

    add db data into env

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
 const mysql2 = require('mysql2')
 const connection = mysql2.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'web_app_movie',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'your_username',
+    password: process.env.DB_PASSWORD || 'your_password',
+    database: process.env.DB_NAME || 'your_database_name',
 })
 
 
